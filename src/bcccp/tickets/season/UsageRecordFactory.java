972db1999aa8fa91058fd8f0fb29ebd9412c5adc
ductien0003 <30862932+ductien0003@users.noreply.github.com>
2017-08-23 14:26:53 +1000

    UsageRecordFactory.java

--- src/bcccp/tickets/season/UsageRecordFactory.java
+++ src/bcccp/tickets/season/UsageRecordFactory.java
@@ -5,6 +5,7 @@ public class UsageRecordFactory implements IUsageRecordFactory {
 	@Override
 	public IUsageRecord make(String ticketId, long startDateTime) {
 		// TODO Auto-generated method stub
+		System.out.print("Ticket ID " +ticketId+" is starting at "+startDateTime);
 		return null;
 	}
 

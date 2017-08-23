1e78dd5f1765b634e0839d526f6fb4131a88a2df
ductien0003 <30862932+ductien0003@users.noreply.github.com>
2017-08-23 19:02:56 +1000

    added constructor
    I ahve added a constructor to check the functionality.

--- src/bcccp/tickets/adhoc/AdhocTicketDAO.java
+++ src/bcccp/tickets/adhoc/AdhocTicketDAO.java
@@ -10,7 +10,8 @@ public class AdhocTicketDAO  implements IAdhocTicketDAO  {
 	
 	//Started coding and checking how to use guthub desktop
 	public AdhocTicketDAO(IAdhocTicketFactory factory) {
-		//TODO Implement constructor
+				//TODO Implement constructor
+				this.factory = factory; //added constructor
 	}
 
 

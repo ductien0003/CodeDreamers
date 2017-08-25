b4f5c50e321990ed366e26af7a6092dbd0598273
ductien0003 <30862932+ductien0003@users.noreply.github.com>
2017-08-25 20:08:01 +1000

    added getter to seasonticket.java
    Getter to get starting time

--- src/bcccp/tickets/season/SeasonTicket.java
+++ src/bcccp/tickets/season/SeasonTicket.java
@@ -41,7 +41,7 @@ public class SeasonTicket implements ISeasonTicket {
 	@Override
 	public long getStartValidPeriod() {
 		// TODO Auto-generated method stub
-		return null;
+		return startValidPeriod;
 	}
 
 	@Override
